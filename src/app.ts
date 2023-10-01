0b0ee8570ec292c63989fc793c47c354bd48e390
HelenELee <HelenELee3@outlook.com>
2023-10-01 09:55:52 +0800

    Test Tracking 2

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,6 @@ function autobind(
 */
 //this is a singleton class - can only create one instance of it.
 
-
 class ProjectState {
   private listeners: any[] = []; //array of functions called when we add a new project
   private projects: any[] = [];
